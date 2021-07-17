d377829ef9b1c1dad3bda0bc024a0c43d74f25be
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-07-17 17:21:17 -0500

    ✨ [add] all CRUD methods to movies

--- routes/movies.js
+++ routes/movies.js
@@ -5,11 +5,56 @@ function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router);
 
+  //Get all movies
   router.get('/', async (req, res, next) => {
     try {
       const movies = await Promise.resolve(moviesMock);
 
       res.status(200).json(movies);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Get a movie by Id
+  router.get('/:movieId', async (req, res, next) => {
+    try {
+      const movie = await Promise.resolve(moviesMock[0]);
+
+      res.status(200).json(movie);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Create a movie
+  router.post('/', async (req, res, next) => {
+    try {
+      const createdMovie = await Promise.resolve(moviesMock[0].id);
+
+      res.status(201).json(createdMovie);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Update movie
+  router.put('/:movieId', async (req, res, next) => {
+    try {
+      const updatedMovie = await Promise.resolve(moviesMock[0]);
+
+      res.status(200).json(updatedMovie);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  //Delete a movie
+  router.delete('/:movieId', async (req, res, next) => {
+    try {
+      const deletedMovie = await Promise.resolve(moviesMock[0].id);
+
+      res.status(200).json(deletedMovie);
     } catch (error) {
       next(error);
     }
