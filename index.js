bf0a6dbb425685c5d10235dfec2cfe995299a679
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-08-01 17:20:34 -0500

    ✨ [add] wrap errors to boom

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const config = require('./config');
 const moviesApi = require('./routes/movies');
-const { logError, errorHandler } = require('./utils/middleware/errorHandler');
+const {
+  logError,
+  errorHandler,
+  wrapErrors,
+} = require('./utils/middleware/errorHandler');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
 const app = express();
 
 //body parser
@@ -9,8 +15,12 @@ app.use(express.json());
 //routes
 moviesApi(app);
 
+//catch 404
+app.use(notFoundHandler);
+
 //Error handler
 app.use(logError);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
--- utils/middleware/errorHandler.js
+++ utils/middleware/errorHandler.js
@@ -1,24 +1,36 @@
+const boom = require('@hapi/boom');
 const config = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    return { ...error, stack };
   }
   return error;
 }
 
 function logError(error, req, res, next) {
   console.error(error);
-  next();
+  next(error);
+}
+
+function wrapErrors(error, req, res, next) {
+  if (!error.isBoom) {
+    next(boom.badImplementation(error));
+  }
+  next(error);
 }
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next) {
-  res.status(error.status || 500);
-  res.json(withErrorStack(error.message, error.stack));
+  const {
+    output: { statusCode, payload },
+  } = error;
+  res.status(statusCode);
+  res.json(withErrorStack(payload, error.stack));
 }
 
 module.exports = {
   logError,
   errorHandler,
+  wrapErrors,
 };
